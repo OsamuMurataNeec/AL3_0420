4f47f6c95b102cf3fafcc2321b559a4c5845d50f
OsamuMurataNeec <murataosa@gmail.com>
2024-04-20 13:57:52 +0900

    2-1のスライド21枚目まで実装

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -7,6 +7,9 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "Player.h"
+
+
 
 /// <summary>
 /// ゲームシーン
@@ -47,4 +50,15 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// 自キャラ
+	Player* player_ = nullptr;
 };
